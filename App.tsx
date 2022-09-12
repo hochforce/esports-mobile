906e66c2b202295da02bf8b1e189934a00b6d91d
Hugo Hoch <hochforce@gmail.com>
2022-09-12 20:28:26 -0300

    Clean react native project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+interface ButtonProps{
+  title: String
+}
+function Button (props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Button title="Clique 1" />
+      <Button title="Clique 2" />
+      <Button title="Clique 3" />
+      <Button title="Hello World" />
+
       <StatusBar style="auto" />
     </View>
   );
